69aea283407fe9bb524eb714c2b8e797671d9ad8
Travis J Parker <travis.parker@gmail.com>
2009-07-11 13:29:07 -0700

    added a Configuration model, this will replace the current config.py

--- tkt/models.py
+++ tkt/models.py
@@ -1,3 +1,7 @@
+import os
+import stat
+import subprocess
+
 import tkt.files
 import tkt.timezones
 import yaml
@@ -40,6 +44,77 @@ class Model(object):
         data = self.yamlable()
         self.timezones_to_local()
         return yaml.dump(data, stream=stream, default_flow_style=False)
+
+class Configuration(Model):
+    fields = ['username', 'useremail', 'datafolder']
+
+    RCFILENAME = '.tktrc.yaml'
+    DEFAULT_DATAFOLDER = '.tkt'
+
+    def __init__(self, data):
+        super(Configuration, self).__init__(data)
+        for fieldname in self.fields:
+            if not getattr(self, fieldname):
+                setattr(self, fieldname,
+                        getattr(self, "default_%s" % fieldname)())
+
+    @classmethod
+    def _find(cls, name):
+        path = os.path.abspath(os.curdir)
+        parentpath = os.path.abspath(os.path.join(path, os.pardir))
+
+        rcpath = os.path.join(path, name)
+        if os.path.exists(rcpath):
+            return rcpath, True
+
+        # the theory is that once we reach the root, the parent folder's path
+        # is the same as the current folder's path
+        while path != parentpath:
+            path = parentpath
+            parentpath = os.path.abspath(os.path.join(path, os.pardir))
+
+            rcpath = os.path.join(path, name)
+            if os.path.exists(rcpath):
+                return rcpath, True
+
+        homedir = os.environ.get('HOME')
+        if not homedir:
+            raise RuntimeError("no %s or HOME directory found" % name)
+
+        rcpath = os.path.abspath(os.path.join(homedir, name))
+        return rcpath, os.path.exists(rcpath)
+
+    @classmethod
+    def rcfile(cls):
+        rcfile, present = cls._find(cls.RCFILENAME)
+        if not present:
+            os.mknod(rcfile, 0644, stat.S_IFREG)
+        return rcfile
+
+    @property
+    def datapath(self):
+        if not hasattr(self, "_datapath"):
+            rc, present = cls._find(self.datafolder)
+            if not present:
+                os.mkdir(rc)
+            self._datapath = rc
+        return self._datapath
+
+    def default_username(self):
+        return (os.environ.get('USER') or 'anonymous user').title()
+
+    def default_useremail(self):
+        if not hasattr(self, 'username'):
+            self.username = self.default_username()
+        try:
+            proc = subprocess.Popen("hostname", stdout=subprocess.PIPE)
+            hostname = proc.communicate()[0].rstrip()
+        except:
+            hostname = "localhost.localdomain"
+        return "%s@%s" % (self.username.lower().replace(" ", "."), hostname)
+
+    def default_datafolder(self):
+        return self.DEFAULT_DATAFOLDER
 
 class Issue(Model):
     fields = [
