b44b3d9c6b019b9c5025e3160b5dd9a55df8bcb7
Travis J Parker <travis.parker@gmail.com>
2009-07-11 23:05:01 -0700

    avoiding an import loop

--- tkt/files.py
+++ tkt/files.py
@@ -1,20 +1,21 @@
 import os
 
-import tkt.config
-
 
 def project_filename():
+    import tkt.config
     return os.path.join(
             tkt.config.config.datapath,
             "project.yaml")
 
 def issue_filename(issueid):
+    import tkt.config
     return os.path.join(
             tkt.config.config.datapath,
             issueid,
             "issue.yaml")
 
 def event_filename(issueid, eventid):
+    import tkt.config
     return os.path.join(
             tkt.config.config.datapath,
             issue.id,
