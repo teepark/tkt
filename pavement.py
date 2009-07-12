ae0262399dfd3728ca3be4d00b94deafead4aa48
Travis J Parker <travis.parker@gmail.com>
2009-07-12 16:35:26 -0700

    set up distutils/paver

---
+++ pavement.py
@@ -0,0 +1,39 @@
+from paver.easy import *
+from paver.path import path
+from paver.setuputils import setup
+
+
+setup(
+    name="tkt",
+    packages=["tkt"],
+    scripts=["scripts/tkt"],
+    version="0.1",
+    author="Travis Parker",
+    author_email="travis.parker@gmail.com"
+)
+
+MANIFEST = (
+    "setup.py",
+    "paver-minilib.zip",
+)
+
+@task
+def manifest():
+    path('MANIFEST.in').write_lines('include %s' % x for x in MANIFEST)
+
+@task
+@needs('generate_setup', 'minilib', 'manifest', 'setuptools.command.sdist')
+def sdist():
+    pass
+
+@task
+def clean():
+    for p in map(path, ('tkt.egg-info', 'dist', 'build', 'MANIFEST.in')):
+        if p.exists():
+            if p.isdir():
+                p.rmtree()
+            else:
+                p.remove()
+    for p in path(__file__).abspath().parent.walkfiles():
+        if p.endswith(".pyc") or p.endswith(".pyo"):
+            p.remove()
