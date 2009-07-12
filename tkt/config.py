60ca9543602b36e2bc63351deabacc84022604f7
Travis J Parker <travis.parker@gmail.com>
2009-07-11 22:31:18 -0700

    gutted the config module to use the new Configuration model, made matching changes to files.py

--- tkt/config.py
+++ tkt/config.py
@@ -1,47 +1,5 @@
-import os
-import subprocess
-
+import tkt.models
 import yaml
 
 
-RCFILENAME = '.tktrc.yaml'
-CONFIGVARS = ['username', 'useremail', 'datafolder']
-
-# first, seach the current directory up to the root
-_foundrcfile = False
-_path = os.path.abspath(os.curdir)
-_parentpath = os.path.abspath(os.path.join(_path, os.pardir))
-while _path != _parentpath:
-    _path = _parentpath
-    _parentpath = os.path.abspath(os.path.join(_path, os.pardir))
-    _rcpath = os.path.join(_path, RCFILENAME)
-    if os.path.exists(_rcpath):
-        globals().update(yaml.load(open(_rcpath)))
-        _foundrcfile = True
-        break
-
-# failing that, try the user's home directory
-if not _foundrcfile:
-    _path = os.path.join(os.environ['HOME'], RCFILENAME)
-    _rcpath = os.path.join(_path, RCFILENAME)
-    if os.path.exists(_rcpath):
-        globals().update(yaml.load(open(_rcpath)))
-        _foundrcfile = True
-
-# finally, do a best guess at values
-if not _foundrcfile:
-    username = os.environ['USER'].title()
-    try:
-        _proc = subprocess.Popen("hostname", stdout=subprocess.PIPE)
-        _hostname = _proc.communicate()[0].rstrip()
-    except:
-        _hostname = "localhost.localdomain"
-    useremail = "%s@%s" % (username.lower(), _hostname)
-    datafolder = '.tkt'
-    _rcpath = os.path.abspath(os.path.join(os.curdir, RCFILENAME))
-
-# make sure we got all the values we needed
-assert set(globals().keys()) >= set(CONFIGVARS), "missing config values: %s" % \
-        ",".join(set(CONFIGVARS).difference(globals().keys()))
-
-datapath = os.path.abspath(os.path.join(_rcpath, os.pardir, datafolder))
+config = tkt.models.Configuration.load(tkt.models.Configuration.rcfile())
--- tkt/files.py
+++ tkt/files.py
@@ -3,5 +3,19 @@ import os
 import tkt.config
 
 
-def filename(objtype, id):
-    return os.path.join(tkt.config.datapath, "%s-%s.yaml" % (objtype, id))
+def project_filename():
+    return os.path.join(
+            tkt.config.config.datapath,
+            "project.yaml")
+
+def issue_filename(issueid):
+    return os.path.join(
+            tkt.config.config.datapath,
+            issueid,
+            "issue.yaml")
+
+def event_filename(issueid, eventid):
+    return os.path.join(
+            tkt.config.config.datapath,
+            issue.id,
+            "%s.yaml" % eventid)
