5c5f94459e2fdedd512312710b21e34ddbf0e6bb
Travis J Parker <travis.parker@gmail.com>
2009-07-30 22:13:48 -0700

    sub-optimal solution for configuration changes in plugins

--- tkt/getplugins.py
+++ tkt/getplugins.py
@@ -6,6 +6,11 @@ import tkt.config
 
 def getplugins():
     plugins = set(tkt.config.user().plugins + tkt.config.project().plugins)
+
+    # we'll have to reload configs, so bust that cache
+    del tkt.config._user
+    del tkt.config._project
+
     for plugin in plugins:
         try:
             # plugins are responsible for attaching to the right hooks
